041afa12fa3fb8da1730b284e7ce81715acec24e
Shrejan Kotyan <shrejankotyan@gmail.com>
2025-07-31 15:57:16 +0530

    mqtt protocall integreted

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-import { log } from "console";
+import mqtt from "mqtt";
 const app = express();
 const PORT = 3000;
 app.use(express.json());
@@ -11,18 +11,47 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: { origin: "*" },
 });
+//username : hivemq.webclient.1753956973528
+//paswerd : Gd41aCBcxW,0eA>%2#bM
+
+
 let busLocations =[];
+
+const options = {
+  host: "d8b8feafe64d4ad98a28e2310525d196.s1.eu.hivemq.cloud",
+  port: 8883,
+  protocol: "mqtts",
+  username: "hivemq.webclient.1753956973528",
+  password: "Gd41aCBcxW,0eA>%2#bM",
+};
+const mqttClient = mqtt.connect(options); // Or your broker URL
+
+mqttClient.on("connect", () => {
+  console.log("Connected to MQTT broker");
+  mqttClient.subscribe("owntracks/+/+"); // Subscribe to all OwnTracks topics
+});
+mqttClient.on("message", (topic, message) => {
+  try {
+    const data = JSON.parse(message.toString());
+    if (data._type === "location") {
+      busLocations.push(data);
+      // console.log("Received via MQTT:", data);
+    }
+  } catch (err) {
+    console.error("Invalid MQTT message:", err);
+  }
+});
 io.on("connection", (socket) => {
   console.log("Client connected once");
 });
-app.post("/location", (req, res) => {
+/*app.post("/location", (req, res) => {
   const data = req.body;
   //busLocations[data.tid || data._id] = data;
 busLocations.push(data);
   //console.log(busLocations);
  
   res.json({ status: "ok" });
-});
+});*/
 setInterval(() => {if(busLocations.length > 0)
   {io.emit("data", busLocations);
   console.log("Emitting bus locations:", busLocations);
@@ -32,3 +61,60 @@ setInterval(() => {if(busLocations.length > 0)
 httpServer.listen(3000,'0.0.0.0', () => {
   console.log("listening on *:3000");
 });
+/*import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import mqtt from "mqtt";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: "*" },
+});
+
+let busLocations = [];
+
+// MQTT SETUP
+const mqttClient = mqtt.connect("mqtt://broker.hivemq.com"); // Or your broker URL
+
+mqttClient.on("connect", () => {
+  console.log("Connected to MQTT broker");
+  mqttClient.subscribe("owntracks/+/+"); // Subscribe to all OwnTracks topics
+});
+
+mqttClient.on("message", (topic, message) => {
+  try {
+    const data = JSON.parse(message.toString());
+    if (data._type === "location") {
+      busLocations.push(data);
+      // console.log("Received via MQTT:", data);
+    }
+  } catch (err) {
+    console.error("Invalid MQTT message:", err);
+  }
+});
+
+// SOCKET.IO: Send to frontend every 3s
+setInterval(() => {
+  if (busLocations.length > 0) {
+    io.emit("data", busLocations);
+    console.log("Emitting bus locations:", busLocations);
+    busLocations = [];
+  }
+}, 3000);
+
+// Socket connection log
+io.on("connection", (socket) => {
+  console.log("Client connected via Socket.io");
+});
+
+// Start server
+httpServer.listen(PORT, "0.0.0.0", () => {
+  console.log("listening on *:3000");
+}); */
